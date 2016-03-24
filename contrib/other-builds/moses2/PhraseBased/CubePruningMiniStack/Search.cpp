daf7a546fe695e0ef3061ebcf9e620917ae27ed3
Hieu Hoang <hieuhoang@gmail.com>
2016-03-24 12:08:18 +0000

    don' use custom allocator for Queue

--- contrib/other-builds/moses2/PhraseBased/CubePruningMiniStack/Search.cpp
+++ contrib/other-builds/moses2/PhraseBased/CubePruningMiniStack/Search.cpp
@@ -32,8 +32,7 @@ Search::Search(Manager &mgr)
 ,m_stack(mgr)
 ,m_cubeEdgeAlloc(mgr.GetPool())
 
-,m_queue(QueueItemOrderer(),
-		std::vector<QueueItem*, MemPoolAllocator<QueueItem*> >(MemPoolAllocator<QueueItem*>(mgr.GetPool())) )
+,m_queue()
 
 ,m_seenPositions(MemPoolAllocator<CubeEdge::SeenPositionItem>(mgr.GetPool()))
 
@@ -79,7 +78,7 @@ void Search::Decode(size_t stackInd)
 	Recycler<HypothesisBase*> &hypoRecycler  = mgr.GetHypoRecycle();
 
 	// reuse queue from previous stack. Clear it first
-	std::vector<QueueItem*, MemPoolAllocator<QueueItem*> > &container = Container(m_queue);
+	std::vector<QueueItem*> &container = Container(m_queue);
 	//cerr << "container=" << container.size() << endl;
 	BOOST_FOREACH(QueueItem *item, container) {
 		// recycle unused hypos from queue
